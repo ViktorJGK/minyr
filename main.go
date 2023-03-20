2d446599845001da13a47ee4679dc426bd05df7f
Viktor J.G Kalhovd <viktorkallhovd02@gmail.com>
2023-03-20 11:44:06 +0100

    ny kode for å lese og konvertere fil, er WIP

--- main.go
+++ main.go
@@ -3,23 +3,59 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
+	"io"
 	"os"
+	"strconv"
 )
 
 func main() {
-	// åpner CSV filen
-	fd, error := os.Open("kjevik-temp-celsius-20220318-20230318.csv")
-	if error != nil {
-		fmt.Println(error)
+	// Åpne csv filen
+	inputFilename := "kjevik-temp-celsius-20220318-20230318.csv"
+	inputFile, err := os.Open(inputFilename)
+	if err != nil {
+		fmt.Printf("kunne ikke åpne filen %s: %s\n", inputFilename, err)
 	}
-	fmt.Println("Åpnet filen uten problemer")
-	defer fd.Close()
+	defer inputFile.Close()
 
-	// Leser CSV filen
-	fileReader := csv.NewReader(fd)
-	records, error := fileReader.ReadAll()
-	if error != nil {
-		fmt.Println(error)
+	//lager CSV-leser og skriver
+	reader := csv.NewReader(inputFile)
+	writer := *csv.NewWriter(outputFile)
+
+	// leser hver rad av inputfilen og konverter cel til fahr og lager ny fil med fahr
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Println("kunne ikke lese rad:", err)
+			continue
+		}
+
+		//converterer celsius til fahrenheit
+		celsius, err := strconv.ParseFloat(record[5], 64)
+		if err != nil {
+			fmt.Println("ugyldig temperaturverdi", err)
+			continue
+		}
+
+		/*
+			CelsiusToFahrenheit
+			record[5] = strconv.FormatFloat(fahrenheit, "f", 2, 64)
+		*/
+
+		//skriver til output filen
+		err = writer.Write(record)
+		if err != nil {
+			fmt.Println("kunne ikke skrive rad:", err)
+			continue
+		}
 	}
-	fmt.Println(records)
+	writer.Flush()
+	if err := writer.Error(); err != nil {
+		fmt.Println("kunne ikke skrive til fil:", err)
+		return
+	}
+
+	fmt.Println("Konveretringen fra celsius til fahrenheit er fulført")
 }
