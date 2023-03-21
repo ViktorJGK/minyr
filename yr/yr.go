d406068304234c14ebde62730976c908e4ac77c0
Viktor J.G Kalhovd <viktorkallhovd02@gmail.com>
2023-03-21 12:59:18 +0100

    nesten laget ferdig convertion for positive tall, må testes

--- yr/yr.go
+++ yr/yr.go
@@ -1,10 +1,11 @@
 package yr
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 
-	"github.com/ViktorJGK/funtemps/conv"
+	_ "github.com/ViktorJGK/funtemps/conv"
 )
 
 func convertionPositiv() {
@@ -18,12 +19,25 @@ func convertionPositiv() {
 		lastPart, err := strconv.ParseFloat(parts[3], 64)
 		if err != nil {
 			panic(err)
-			conv.CelsiusToFahrenheit(cel)
-
 		}
+
+		celsius := conv.CelsiusToFahrenheit(lastPart)
+		fmt.Printf("%.2f er det samme som %.2f i fahrenheit\n", lastPart, fahrenheit)
+
+		// gjør at det er kunn 1 decimal plass
+		lastPartStr := fmt.Sprintf("%.1f", lastPart)
+
+		// kobler alle delene sammen igjen
+		parts[3] = lastPartStr
+		outputStr := strings.Join(parts, ";")
+
+		//Forande slik at den gjør endringene i ny fil
+		fmt.Println(outputStr)
 	}
 }
 
 func convertionNull() {}
 
 func convertionNegativ() {}
+
+func convTextSlutt() {}
