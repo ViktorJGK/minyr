fdbb0b14dd4022b3c61aa1895433fd0748597c5f
Viktor J.G Kalhovd <viktorkallhovd02@gmail.com>
2023-03-21 12:29:18 +0100

    jobber med convertering fil og import av tidligere oppgave funtemps conv metode

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ViktorJGK/funtemps/conv
+
+go 1.19
--- yr/yr.go
+++ yr/yr.go
@@ -1,17 +1,29 @@
 package yr
 
-func convert1() {
+import (
+	"strconv"
+	"strings"
 
+	"github.com/ViktorJGK/funtemps/conv"
+)
+
+func convertionPositiv() {
+	inputArr := []string{}
+	for i := range inputArr {
+		inputStr := inputArr[i]
+
+		//deler innput i strengen mellom ;
+		parts := strings.Split(inputStr, ";")
+
+		lastPart, err := strconv.ParseFloat(parts[3], 64)
+		if err != nil {
+			panic(err)
+			conv.CelsiusToFahrenheit(cel)
+
+		}
+	}
 }
 
-func convert2() {
+func convertionNull() {}
 
-}
-
-func convert3() {
-
-}
-
-func convTextSlutt(string string) string {
-
-}
+func convertionNegativ() {}
